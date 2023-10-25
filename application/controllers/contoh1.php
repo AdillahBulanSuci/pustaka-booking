747a1eb9606c089bbc8673093a1e0084e1738ebe
AdillahBulanSuci <adillahbulan@gmail.com>
2023-10-25 23:36:21 +0700

    menambahkan semua

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_controller
+{
+    public function index()
+    {
+        echo "Nama Saya Adillah Bulan Suci <br>";
+        echo"Saya tinggal di Tegal";
+    }
+}
+
+?>
